9aa60482ba67a33410099d3642d8df2a8031976f
Abdullah <abdullahwindows10user@gmail.com>
2023-07-14 23:45:29 +0500

    Major Changse

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 let topNav = document.getElementById("top-nav");
+let topNavUl = document.getElementById("top-nav").firstElementChild;
 let toggleNav = document.getElementById("nav-toggle-svg");
 
 toggleNav.addEventListener("click", (e) => {
-    topNav.classList.toggle("top-0");
+    topNavUl.classList.toggle("flex");
+    if (topNav.style.width == "100%") {
+        topNav.style.animation = "width-reverse 100ms linear";
+        topNav.style.width = 0;
+    }
+    else {
+        topNav.style.animation = "width 100ms linear";
+        topNav.style.width = "100%";
+    }
 });
